3700ba6f3e97525df400262cc767b25f146d9d57
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-05 03:06:13 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,8 @@ require_relative './class_methods_module.rb'
 require_relative 'fancy_dance'
 
 class Kid 
-  include Dance 
-  extend MetaDancing
+  include FancyDance::InstanceMethods 
+  extend FancyDance::ClassMethods
   attr_accessor :name 
   
   def initialize(name)
